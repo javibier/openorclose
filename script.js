77b0645d7932712a082aef226fcd5566aa50036e
Javier Echevarria <javierechevarria21@yahoo.com>
2020-05-11 15:05:39 -0400

    revise js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var time = new Date() .getHours();
 var message;
 
 
-var openOrClosedJS = document.getElementById ("openorClosed");
+var openOrClosedJS = document.getElementById ("openOrClosed");
 
 
 
-if (time &lt; 11 || > time &gt; > 21)
+if (time &lt; 11 || time &gt; 21)
 {
 	message = "Closed";
 }
