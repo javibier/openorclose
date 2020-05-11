87ad18fbd3ca534c71af96ca7e98f228bbd3bc4d
Javier Echevarria <javierechevarria21@yahoo.com>
2020-05-11 14:38:18 -0400

    add if stmnt js

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ var openorClosedJS = document.getElementById ("openorClosed");
 var time = new Date () .getHours 
 
 var message;
+
+if (time < 11 || > time > 21)
+{
+	message = "Closed"
+}
+else
+{
+	message = "Open"
+}
